02823d1a6065cdfe0e43db4f7496d1df6b0bd604
Josef Kolář <thejoeejoee@gmail.com>
2022-08-20 21:07:33 +0200

    Fix CSS deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,11 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    target: "static",
     modules: [
         '@nuxtjs/tailwindcss',
     ],
     css: [
-        '@/assets/main.css',
+        '@/assets/css/main.css',
     ],
-
 })
